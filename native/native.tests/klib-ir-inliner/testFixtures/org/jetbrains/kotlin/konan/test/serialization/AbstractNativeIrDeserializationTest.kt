ac167408d3a59a21b85cc580020c23435ce38aa9
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2026-02-09 14:58:10 +0100

    [Native][Tests] Split NativeEnvironmentConfigurator to two configurators

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/serialization/AbstractNativeIrDeserializationTest.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/serialization/AbstractNativeIrDeserializationTest.kt
@@ -28,7 +28,7 @@ import org.jetbrains.kotlin.test.frontend.fir.FirMetaInfoDiffSuppressor
 import org.jetbrains.kotlin.test.frontend.fir.FirOutputArtifact
 import org.jetbrains.kotlin.test.model.*
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerWithTargetBackendTest
-import org.jetbrains.kotlin.test.services.configuration.NativeEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.configuration.NativeFirstStageEnvironmentConfigurator
 
 // Base class for IR serialization/deserialization test, configured with FIR frontend, in Native-specific way.
 open class AbstractNativeIrDeserializationTest : AbstractKotlinCompilerWithTargetBackendTest(TargetBackend.NATIVE) {
@@ -46,7 +46,7 @@ open class AbstractNativeIrDeserializationTest : AbstractKotlinCompilerWithTarge
         get() = ::NativeDeserializerFacade
 
     override fun configure(builder: TestConfigurationBuilder) = with(builder) {
-        useConfigurators(::NativeEnvironmentConfigurator)
+        useConfigurators(::NativeFirstStageEnvironmentConfigurator)
         useAfterAnalysisCheckers(::FirMetaInfoDiffSuppressor)
         commonConfigurationForNativeFirstStageUpToSerialization(
             targetFrontend,
