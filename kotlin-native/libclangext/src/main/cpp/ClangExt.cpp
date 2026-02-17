e18224201365e77085cd42ed2a7ad650e673292c
Igor Chevdar <igor.chevdar@jetbrains.com>
2026-02-17 15:43:01 +0100

    fixup! Fix for Windows

--- kotlin-native/libclangext/src/main/cpp/ClangExt.cpp
+++ kotlin-native/libclangext/src/main/cpp/ClangExt.cpp
@@ -18,14 +18,16 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <llvm/ADT/StringRef.h>
+#include "clang-c/ext.h"
+
 #if LIBCLANGEXT_ENABLE
 #include <clang/AST/Attr.h>
 #include <clang/AST/DeclObjC.h>
 #include <clang/Frontend/ASTUnit.h>
-#endif
-#include "clang-c/ext.h"
 
 using namespace clang;
+#endif
 
 #if LIBCLANGEXT_ENABLE
 
@@ -75,7 +77,7 @@ static CXTypeAttributes makeCXTypeAttributes() {
 
 #endif // LIBCLANGEXT_ENABLE
 
-static CString createCString(StringRef str) {
+static CString createCString(llvm::StringRef str) {
   return CString { strdup(str.str().c_str()) };
 }
 
